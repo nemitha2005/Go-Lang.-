039eb1878b130978a2e7da180752dc9f1480206a
Nemitha Wijerathna <nemithan05@gmail.com>
2024-06-18 14:47:43 +0530

    golang practice 18June

---
+++ golangbasics.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    //fmt.Println("Hello, World!")
+
+    /*
+    var intNum int = 32
+    fmt.Println("intNum = ", intNum)
+    */
+
+    /*
+    var y string = "Hello, World!"
+    fmt.Println(y)
+
+    var x int = 32
+    var z string = "Nemitha"
+    var a float32 = 3.5
+    var b bool = true
+    
+    fmt.Println(x)
+    fmt.Println(z)
+    fmt.Println(a)
+    fmt.Println(b) 
+    
+    
+    var intNum int = 32767
+    intNum = intNum + 1
+    fmt.Println(intNum)
+
+    var floatNum float64 = 12345678.9
+    fmt.Println(floatNum)
+
+    var floatNum32 float32 = 10.1
+    var intNum32 int32 = 2
+    var result float32 = floatNum32 + float32(intNum32)
+    fmt.Println(result)
+
+    var intNum1 int = 3
+    var intNum2 int = 4
+    fmt.Println(intNum1 / intNum2)
+    fmt.Println(intNum1 % intNum2)
+    */
+
+    var myString string = "Hello \nWorld!"
+    fmt.Println(myString)//line break
+
+    var myString1 string = "Hello \tWorld!"
+    fmt.Println(myString1)//tab space
+
+    
+}
